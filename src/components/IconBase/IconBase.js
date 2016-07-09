b1edaee60a46b76da3fadbe6e9467d86bc361528
Felix Astner <me@felixastner.de>
2016-07-09 08:07:21 +0200

    fixed iconbase classname

--- src/components/IconBase/IconBase.js
+++ src/components/IconBase/IconBase.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class IconBase extends React.Component {
     render() {
-        return <span className="icon">
+        return <span className="icon-panel">
             <svg  className="icon" version="1.1" x="0px" y="0px"
 	 viewBox="0 0 512 512" enable-background="new 0 0 512 512">
             {this.props.children}
